de41b9f770f1a883ea94d6966f945a60e8c89fb6
limingfang <limingfang97@outlook.com>
2022-03-09 20:36:34 +0800

    完成“邓氏鱼”的应用程序部分代码

---
+++ user/src/lib.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![feature(linkage)]
+#![feature(panic_info_message)]
+
+mod console;
+mod syscall;
+use core::panic::PanicInfo;
+use syscall::*;
+
+#[panic_handler]
+fn my_panic_handler(info: &PanicInfo) -> ! {
+    if let Some(loc) = info.location() {
+        println!(
+            "panicked at {}:{} {}",
+            loc.file(),
+            loc.line(),
+            info.message().unwrap(),
+        );
+    } else {
+        println!("panicked: {}", info.message().unwrap());
+    }
+    loop {}
+}
+
+#[no_mangle]
+#[link_section = ".text.start"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("shouldn't go here!");
+}
+
+#[no_mangle]
+#[linkage = "weak"]
+fn main() -> isize {
+    panic!("Can't find main!!!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|b| unsafe {
+        (b as *mut u8).write_volatile(0);
+    })
+}
---
+++ user/src/syscall.rs
@@ -0,0 +1,20 @@
+use core::arch::asm;
+
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+fn sys_call(syscall_id: usize, arg0: usize, arg1: usize, arg2: usize) -> isize {
+    let ret;
+    unsafe {
+        asm!("ecall", inlateout("x10") arg0 => ret,in("x11") arg1,in("x12") arg2,in("x17") syscall_id);
+    }
+    ret
+}
+
+pub fn exit(status: isize) {
+    sys_call(SYSCALL_EXIT, 0, 0, 0);
+}
+
+pub fn write(fd: usize, buffer: &[u8]) -> isize {
+    sys_call(SYSCALL_WRITE, fd, buffer.as_ptr() as usize, buffer.len())
+}
